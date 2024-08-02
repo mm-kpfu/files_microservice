5603ae5d768c3e89e966cc54426072e858d9cc0a
MarselMuzafarov <muzafarov.olymp@mail.ru>
2024-08-02 21:36:40 +0300

    Handle parse error

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,17 @@
 from typing import Tuple
 
-from fastapi import Request, UploadFile
+from fastapi import Request, UploadFile, HTTPException
+from fastapi import status
+from starlette.formparsers import MultiPartException
 
 from src.parsers import TargetFileMultipartParser
 
 
 async def parse_files_from_request(request: Request) -> Tuple[UploadFile, ...]:
-    parser = TargetFileMultipartParser(request.headers, request.stream())
-    form_data = await parser.parse()
-    files = tuple(form_data.values())
+    try:
+        parser = TargetFileMultipartParser(request.headers, request.stream())
+        form_data = await parser.parse()
+        files = tuple(form_data.values())
+    except MultiPartException:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail='Unable to parse file(s)')
     return files
